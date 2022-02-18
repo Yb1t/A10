8176e1f55882df7c6de5ba8c0e0ffb5dd45bb306
hao <1061477968@qq.com>
2022-02-18 20:30:15 +0800

    显示图片的HSV色彩空间（3D）

--- deeplearn/tools/hsvShow.py
+++ deeplearn/tools/hsvShow.py
@@ -7,20 +7,20 @@ from matplotlib import colors
 img = cv2.imread('img.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-h,s,v = cv2.split(hsv_img)
-fig = plt.figure(figsize=(8,6),dpi=80)
-axis = fig.add_subplot(1,1,1,projection="3d")
+h, s, v = cv2.split(hsv_img)
+fig = plt.figure(figsize=(8, 6), dpi=80)
+axis = fig.add_subplot(1, 1, 1, projection="3d")
 
 # 像素颜色设置
-pixel_colors = img.reshape((np.shape(img)[0]*np.shape(img)[1],3))
+pixel_colors = img.reshape((np.shape(img)[0] * np.shape(img)[1], 3))
 # 归一化
-norm = colors.Normalize(vmin=-1.,vmax=1.)
+norm = colors.Normalize(vmin=-1., vmax=1.)
 norm.autoscale(pixel_colors)
 # 转换成list
 pixel_colors = norm(pixel_colors).tolist()
 
 # 显示三维散点图
-axis.scatter(h.flatten(),s.flatten(),v.flatten(),facecolors=pixel_colors,marker='.')
+axis.scatter(h.flatten(), s.flatten(), v.flatten(), facecolors=pixel_colors, marker='.')
 axis.set_xlabel("hue")
 axis.set_ylabel("saturation")
 axis.set_zlabel("value")
